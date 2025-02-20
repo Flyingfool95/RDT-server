6d7bedc357f8a7db854ab6d4aef61fef5b8d07cb
Flyingfool95 <jhernehult@gmail.com>
2025-02-20 19:41:05 +0100

    Setting up routing with Login.tsx route

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,12 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./routes/Login";
 
 function App() {
     return (
-        <>
-            <h1>App</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur cupiditate sequi natus voluptatem consequatur harum adipisci aperiam incidunt reprehenderit, labore autem magnam neque dolorem soluta eum, optio fuga ad asperiores? Quas possimus reprehenderit, dolore autem odio minus unde explicabo natus ipsam omnis ipsa excepturi earum perspiciatis aspernatur voluptatem consequuntur officia necessitatibus animi reiciendis consectetur libero culpa quisquam voluptates provident. Ipsum quod culpa harum earum quo alias laborum omnis fugit! Nostrum esse quasi consectetur modi excepturi repudiandae, beatae et reprehenderit maiores voluptatibus. Obcaecati dignissimos natus nostrum aperiam nesciunt sit eligendi corporis amet tempore. Sapiente, quam debitis. Excepturi assumenda commodi consequuntur quae.</p>
-        </>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+        </Routes>
     );
 }
 
