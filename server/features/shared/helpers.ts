1454404c1353cc72b5573b979c7155f216841af3
Flyingfool95 <jhernehult@gmail.com>
2025-02-25 18:11:42 +0100

    Adding errors throwing to validateData - Importing xss package for sanitizeString

--- server/features/shared/helpers.ts
+++ server/features/shared/helpers.ts
@@ -1,5 +1,7 @@
 import { Context } from "jsr:@oak/oak";
 import { ZodType, ZodError } from "https://deno.land/x/zod@v3.23.8/mod.ts";
+import xss from "npm:xss";
+import { HttpError } from "./classes.ts";
 
 export function sendResponse(ctx: Context, status: number, data: unknown = null, errors: string[] | null = null) {
     ctx.response.status = status;
@@ -10,19 +12,20 @@ export function sendResponse(ctx: Context, status: number, data: unknown = null,
     };
 }
 
-export function sanitizeData(data: unknown) {}
+export function sanitizeString(data: unknown) {}
 
 export function validateData<T>(zodSchema: ZodType<T>, data: unknown) {
     try {
         return zodSchema.parse(data);
     } catch (error) {
         if (error instanceof ZodError) {
-            //throw error
-            console.error(error.errors);
-            throw new Error(error.errors.map((err) => err.message).join(" and "));
+            throw new HttpError(
+                400,
+                "Validation error",
+                error.errors.map((err) => err.message)
+            );
         } else {
-            //throw error
-            console.error("Unknown error", error);
+            throw new Error(error instanceof Error ? error.message : "Unknown error");
         }
     }
 }
--- shared/zod/auth.js
+++ shared/zod/auth.js
@@ -4,15 +4,18 @@ const z =
         ? await import("https://deno.land/x/zod@v3.23.8/mod.ts").then((mod) => mod.z)
         : await import("zod").then((mod) => mod.z);
 
-export const loginSchema = z.object({
-    email: z.string().email("Invalid email format"),
-    password: z.string().min(8, "Password must be at least 8 characters"),
-});
+export const loginSchema = z
+    .object({
+        email: z.string().email("Invalid email format"),
+        password: z.string().min(8, "Password must be at least 8 characters"),
+    })
+    .strict();
 
 export const registerSchema = loginSchema
     .extend({
         confirmPassword: z.string(),
     })
+    .strict()
     .refine((data) => data.password === data.confirmPassword, {
         message: "Passwords do not match",
         path: ["confirmPassword"],
