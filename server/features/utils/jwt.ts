ea5f4b900f3fcb42064631073f08e63485aa1670
Flyingfool95 <jhernehult@gmail.com>
2025-03-03 18:37:11 +0100

    Adding jwt key secret to env file

--- server/features/utils/jwt.ts
+++ server/features/utils/jwt.ts
@@ -2,7 +2,19 @@ import { Algorithm } from "https://deno.land/x/djwt@v3.0.2/algorithm.ts";
 import { create, verify, getNumericDate } from "https://deno.land/x/djwt@v3.0.2/mod.ts";
 import { HttpError } from "./classes.ts";
 
-const key = await crypto.subtle.generateKey({ name: "HMAC", hash: "SHA-512" }, true, ["sign", "verify"]);
+const secret = Deno.env.get("JWT_SECRET");
+
+if (!secret) {
+    throw new Error("JWT_SECRET is not set in environment variables.");
+}
+
+const key = await crypto.subtle.importKey(
+    "raw",
+    new TextEncoder().encode(secret),
+    { name: "HMAC", hash: "SHA-512" },
+    true,
+    ["sign", "verify"]
+);
 
 export async function generateJWT(payload: Record<string, unknown>, expiresIn: number = 3600): Promise<string> {
     const header = { alg: "HS512" as Algorithm, typ: "JWT" };
