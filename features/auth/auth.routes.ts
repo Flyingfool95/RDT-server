205937c3c47214bfe007ff78bc35ef157fc093f8
Flyingfool95 <jhernehult@gmail.com>
2025-04-09 19:41:12 +0200

    Re-ordering the aut.routes endpoints

--- features/auth/auth.routes.ts
+++ features/auth/auth.routes.ts
@@ -10,13 +10,16 @@ import { resetPassword } from "./controllers/reset_password.controller.ts";
 import { sendResetEmail } from "./controllers/send_reset_email.controller.ts";
 
 const authRoutes = new Router();
+
 authRoutes.post("/register", rateLimiter, register);
 authRoutes.post("/login", rateLimiter, login);
-authRoutes.put("/update", update);
-authRoutes.get("/logout", logout);
-authRoutes.delete("/delete", deleteUser);
-authRoutes.get("/auth-check", authCheck);
 authRoutes.post("/reset-password", rateLimiter, resetPassword);
 authRoutes.post("/send-reset-email", rateLimiter, sendResetEmail);
+
+authRoutes.put("/update", update);
+authRoutes.get("/auth-check", authCheck);
+
+authRoutes.delete("/delete", deleteUser);
+authRoutes.get("/logout", logout);
 
 export default authRoutes;
