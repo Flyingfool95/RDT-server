b832b398b4c5e4136075c20855ed84fb8a75ebf5
Flyingfool95 <jhernehult@gmail.com>
2025-06-08 09:55:49 +0200

    REmoving domain stuff from HMS project

--- db/db.ts
+++ db/db.ts
@@ -21,29 +21,6 @@ db.execute(`
   )
 `);
 
-db.execute(`
-  CREATE TABLE IF NOT EXISTS domain (
-    id UUID PRIMARY KEY,
-    name TEXT NOT NULL,
-    description TEXT,
-    created_at TEXT DEFAULT (datetime('now','localtime')),
-    image TEXT
-  )
-`);
-
-db.execute(`
-  CREATE TABLE IF NOT EXISTS domain_user (
-    domain_id UUID NOT NULL,
-    user_id UUID NOT NULL,
-    role TEXT NOT NULL CHECK (
-      role IN ('user', 'admin', 'super_admin')
-    ),
-    PRIMARY KEY (domain_id, user_id),
-    FOREIGN KEY (domain_id) REFERENCES domain(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
-  )
-`);
-
 function closeDb() {
     console.log("Closing database...");
     db.close();
