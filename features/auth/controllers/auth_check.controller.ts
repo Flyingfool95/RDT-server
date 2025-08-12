74ffabf1ee51359a42662dc858b4f577a37e709f
FlyingFool95 <jhernehult@gmail.com>
2025-08-12 11:42:24 +0200

    Clear git cache

--- features/auth/controllers/auth_check.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Context } from "jsr:@oak/oak";
-import { sendResponse } from "../../utils/helpers/helpers.ts";
-
-export function authCheck(ctx: Context) {
-    sendResponse(ctx, 200, { data: { user: ctx.state.user } });
-}
