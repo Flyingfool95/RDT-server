0809642591f19e59af983617d3708b4f984def56
FlyingFool95 <jhernehult@gmail.com>
2025-08-17 13:35:57 +0200

    removing authorization form Expose-Headers

--- middlewares/cors.middleware.ts
+++ middlewares/cors.middleware.ts
@@ -11,8 +11,8 @@ const corsMiddleware: Middleware = async (ctx: Context, next: Next) => {
         ctx.response.headers.set("Access-Control-Allow-Credentials", "true");
         ctx.response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
         ctx.response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        ctx.response.headers.set("Access-Control-Expose-Headers", "Content-Type, Authorization");
-    } else {
+        ctx.response.headers.set("Access-Control-Expose-Headers", "Content-Type");
+    } else if (origin) {
         await logMessage("warn", `Unauthorized origin attempt: ${origin}`, { clientIp: ctx.request.ip });
     }
 
